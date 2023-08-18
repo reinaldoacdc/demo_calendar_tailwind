8c870aa82dc4c576acf54702eb136322a6c3c4ca
reinaldocosta <reinaldo.costa@gentrop.com>
2023-08-18 12:46:59 -0300

    adjust table view by hour

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
+    # where(date = Date.todat).orderBy(start_date)
     @meetings = Meeting.all
     hh = Hash.new
     (8..23).each do |h| 
@@ -8,17 +9,27 @@ class PagesController < ApplicationController
       hh[h][1] = Array.new
     end
 
-
-    @meetings.group_by_minute() do |u|
-      hour = u.start_time.strftime('%H').to_i
-      staffId = u.user_id.to_i
-
-      hh[hour][staffId] << u
-      u.start_time
+    @testes = Hash.new
+    #Each staff has its own array
+    @testes[1] = Array.new
+    @testes[2] = Array.new
+    
+    @meetings.each do |u| 
+      puts u 
+      @testes[u.user] << u
     end
 
+
+   # @meetings.group_by_minute() do |u|
+   #   hour = u.start_time.strftime('%H').to_i
+   #   staffId = u.user.to_i
+   #
+   #   hh[hour][staffId] << u
+   #   u.start_time
+   # end
+
     #Devolver uma matriz da seguinte forma matriz = [ 800: [ [], []  ], 900: [], 930: [] ]
-    puts hh
+    puts @testes
 
     #@bookings = hh
     @bookings = Meeting.all
